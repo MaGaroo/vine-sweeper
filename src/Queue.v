b9dd55d0b01d064551b29e566b5ae48396c485da
kazemf78 <kazemf78@yahoo.com>
2019-05-02 22:49:42 +0430

    new queue implemented

--- src/Queue.v
+++ src/Queue.v
@@ -1,20 +1,21 @@
-module Queue #(parameter msg_bit_width, parameter max_msg) (read, read_en, write_ack, write, write_en, read_ack, clock);
+module Queue #(parameter msg_bit_width, parameter max_msg) (read, read_en, write_ack, write, write_en, read_ack);
 	parameter max_width = 1 << msg_bit_width;
 	output [max_msg * msg_width - 1:0] read;
 	output write_ack;
 	output reg read_en;
 	input [max_msg * msg_width - 1:0] write;
 	input read_ack;
-	input clock;
+//	input clock;
 	
 	reg [msg_bit_width - 1:0] q_in;
 	reg [msg_bit_width - 1:0] q_out;
 	
 	reg [msg_bit_width - 1:0] q [max_msg];
-	
-	always @(posedge clock) 
-	begin
-		if (write_en && q_in != q_out)
+
+
+	always @(posedge write_en) 
+	begin		
+	if (q_in != q_out)
 		begin
 			q[q_in] <= write;
 			q_in <= (q_in + 1) % max_msg;
@@ -22,7 +23,12 @@ module Queue #(parameter msg_bit_width, parameter max_msg) (read, read_en, write
 		end
 		else
 			write_ack <= 1'b0;
-		if (read_ack && (q_in != q_out)) 
+	end
+	
+
+	always @(posedge read_ack)
+	begin
+		if (q_in != q_out)
 		begin
 			read <= q[q_out];
 			q_out <= (q_out + 1) % max_msg;
@@ -31,8 +37,6 @@ module Queue #(parameter msg_bit_width, parameter max_msg) (read, read_en, write
 		else 
 			read_en <= 1'b0;
 	end
-	
-	
 	
 	
 endmodule
--- src/buffer.v
+++ src/buffer.v
@@ -38,12 +38,15 @@ module Buffer #(parameter N = 16, ADDR_WIDTH = 4, BUFFER_SIZE = 4)
 		);
 
 	reg [7:0] ri, rj, rstatus, ti, tj, tstatus;
-	integer rxcnt;
+	reg readytosendtouart;
+	integer rxcnt, txcnt;
 
 	reg [MESSAGE_WIDTH-1:0] rxmsg_fromuart, rxmsg_fromcell,  txmsg_touart, txmsg_tocell;
 
 	initial begin
 		rxcnt = 0;
+		readytosendtouart = 0;
+		// TODO initialize queue variables
 	end
 
 
@@ -51,22 +54,71 @@ module Buffer #(parameter N = 16, ADDR_WIDTH = 4, BUFFER_SIZE = 4)
 	begin
 
 		// receive i, j, status from UART
-		if (rxfinish)
+		if (rxfinish && rxcnt < 3)
 		begin
 			case (rxcnt)
 				0: 	ri <= rxdata;
 				1: 	rj <= rxdata;
 				2: 	rstatus <= rxdata;
 			endcase
-			rxcnt <= rxcnt + 1;
-			if (rxcnt == 3) 
-			begin
-				// TODO push to Queue
+			if (rxcnt == 2)
+			begin 
+				writemsgfromuart <= {ri[ADDR_WIDTH:0], rj[ADDR_WIDTH:0], rstatus[3:0]};
+				fromuart_writeen <= 1;
+				if (fromuart_writeack == 1'b1)
+				begin
+					rxcnt = 0;
+					fromuart_writeen = 0;
+				end
 			end
 		end
 
+		/*if (rxcnt == 3)
+		begin
+			if (~fromuart_writeen)
+			begin
+				writemsgfromuart <= {ri[ADDR_WIDTH:0], rj[ADDR_WIDTH:0], rstatus[3:0]};
+				fromuart_writeen <= 1'b1;
+			end
+			if (fromuart_writeack)
+			begin
+				rxcnt = 0;
+			end
+		end*/
+
 		// transmit i, j, status from Queue to UART
-		// TODO ti, tj, tstatus assignment from Queue	
+		// TODO ti, tj, tstatus assignment from Queue
+		
+		if (txcnt < 3)
+		begin
+			if (~readytosendtouart)
+			begin
+				if (fromcell_readen)
+				begin
+					fromcell_readack <= 1;
+					readytosendtouart <= 1;
+					tstatus <= readmsgfromcell[3:0];
+					tj <= readmsgfromcell[ADDR_WIDTH+4:4];
+					ti <= readmsgfromcell[MESSAGE_WIDTH-1:ADDR_WIDTH+5];
+				end
+			end
+			
+			if (readytosendtouart)
+			begin
+				fromcell_readack <= 0;
+				case (txcnt)
+				begin
+					0: 	begin
+							send = 1;
+
+						end
+				end
+			end
+			else
+			begin
+					
+			end
+		end
 		
 
 		// transmit MESSAGE from cell to buffer
